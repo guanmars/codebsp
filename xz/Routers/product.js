5faa8f899492f8a21a4dd19a3c154d04ddb7872a
guanxun <guanxun@163.com>
2019-07-09 21:10:25 +0800

    add all file

--- xz/Routers/product.js
+++ xz/Routers/product.js
@@ -1,99 +1,54 @@
 const express=require('express');
 const pool=require('../pool.js');
 var router=express.Router();
-//商品列表路由： get   /list 
-router.get('/list',function(req,res){
-  //验证
-	var obj=req.query;
-	var count=parseInt(obj.count),
-		pno=parseInt(obj.pno);
-	if(!count){count=5;}
-	if(!pno){pno=1;}
-	var start=(count-1)*pno;
-  //查询MySQL
-    pool.query('SELECT *FROM xz_laptop Limit ?,?',[start,count],function(err,result){
-		if(err)throw err;
-        if(result.length>0)
-		{res.send(result);return;}
-		if(result.length===0)
-		{res.send({code:201,msg:'err:not find'});}
-    });
-});
-
-//商品详情路由： get   /detail
-router.get('/detail',function(req,res){
-	var obj=req.query;
-  //验证
-    if(!obj.lid)
-	{
-		res.send({code:401,msg:'lid required'});
-		return;
-	}
-  //查询MySQL
-   pool.query('SELECT *FROM xz_laptop WHERE lid=?',[obj.lid],function(err,result){
-	    if(err)throw err;
-		if(result.length>0)
-	   {
-		res.send(result);return;
-	   }
-	   else if(result.length===0)
-	   {
-		res.send({code:201,msg:'err:not find'});
-	   }
-   });
-});
-//删除商品路由： get   /delete
-router.get('/delete',function(req,res){
-	var obj=req.query;
-  //验证
-	if(!obj.lid)
-	{
-		res.send({code:401,msg:'lid required'});
-		return;
-	}
-  //查询MySQL
-    pool.query('DELETE FROM xz_laptop WHERE lid=?',[obj.lid],function(err,result){
-		if(err)throw err;
-		if(result.affectedRows>0)
-		{
-			res.send({code:200,msg:'delete suc'});
-			return;
-		}
-		else if(result.affectedRows===0)
-		{
-			res.send({code:201,msg:'err:delete defeat'});
-			return;
-		}
-    });
-});
-//添加商品路由： post  /add
-router.post('/add',function(req,res){
+//商品的添加路由： post  /v1/add
+router.post('/v1/add',function(req,res){
 	var obj=req.body;
-  //验证
-	var i=400;
-	for(var arr in obj)
-	{
-		i++;
-		if(!obj[arr])
-		{
-			res.send({code:i,msg:'required '+arr});
-			return;
-		}
-	}
-  //修改MySQL data
-    pool.query('INSERT INTO xz_laptop SET ?',[obj],function(err,result){
+	var sql="insert into xz_laptop set ?";
+	pool.query(sql,[obj],function(err,result){
 		if(err)throw err;
-		if(result.affectedRows>0)
-		{
-			res.send({code:200,msg:'update suc'});
-			return;
-		}
-		else if(result.affectedRows===0)
-		{
-			res.send({code:201,msg:'err:update defeat'});
-			return;
-		}
-    });
+		if(result.affectedRows>0){res.send("1");}
+		else{res.send("0");}
+	});
+});
+//商品列表路由： post   /v1/list 
+router.post('/v1/list',function(req,res){
+	var sql="select *from xz_laptop";
+	pool.query(sql,function(err,result){
+		if(err)throw err;
+		res.send(result);
+	});
+});
+//商品分页查询：post  /v1/plist
+router.post('/v1/plist',function(req,res){
+	var $start=parseInt(req.body.start);
+	var $count=parseInt(req.body.count);
+	var sql="select *from xz_laptop limit ?,?";
+	pool.query(sql,[$start,$count],function(err,result){
+		if(err)throw err;
+		res.send(result);
+	});
+});
+//商品删除：delete  /v1/delproduct/:lid
+router.delete('/v1/delproduct/:lid',function(){
+	var $lid=req.params.lid;
+	var sql="delete xz_laptop where lid=?";
+	pool.query(sql,[$lid],function(err,result){
+		if(err)throw err;
+		if(result.affectedRows>0){res.send("1");}
+		else{res.send("0");}
+	});
+});
+//商品修改： put  /v1/put
+router.put('/v1/put',function(req,res){
+	var obj=req.body;
+	var $lid=obj.lid;
+	    delete obj.lid;
+	var sql="update xz_laptop set ? where lid=?";
+	pool.query(sql,[obj,$lid],function(err,result){
+		if(err)throw err;
+		res.send("1");
+	});
 });
 //导出router
 module.exports=router;
