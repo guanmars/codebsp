8f220138a87aed70735d8ab0baeae9f8714e1d82
guanxun <guanxun@163.com>
2019-07-14 17:13:03 +0800

    add all

--- xz/Routers/product.js
+++ xz/Routers/product.js
@@ -4,28 +4,39 @@ var router=express.Router();
 //商品家族添加：post    /v1/addfy
 router.post('/v1/addfy',function(req,res){
 	var $fname=req.body.fname;
-	console.log($fname);
 	var sql="insert into xz_laptop_family values(?,?)";
 	pool.query(sql,[null,$fname],function(err,result){
 		if(err)throw err;
-		if(result.affectedRows>0){
+		if(result.affectedRows>0){res.send("1");
+			/*
 			var sql="select * from xz_laptop_family order by fid DESC limit 1";
 		    pool.query(sql,function(err,result){
 				if(err)throw err;
 				res.send(result);
 		    });
+			*/
 		}
+		else{res.send("0");}
+	});
+});
+//查询某个家族信息：get /v1/schfy/:fid
+router.get('/v1/schfy:fid',function(req,res){
+	var $fid=req.params.fid;
+	var sql="select * from xz_laptop_family where fid=?";
+	pool.query(sql,[$fid],function(err,result){
+		if(err)throw err;
+		if(result.length>0){res.send(result);}
 		else{res.send("0");}
 	});
 });
 //修改商品家族：put   /v1/updfy
 router.put('/v1/updfy',function(req,res){
-	var $fid=req.body.fid;
-	var $fname=req.body.fname;
+	var $fid=req.body.ufid;
+	var $fname=req.body.ufname;
 	sql="update xz_laptop_family set fname=? where fid=?";
 	pool.query(sql,[$fname,$fid],function(err,result){
 		if(err)throw err;
-		if(resutl.affectedRows>0){res.send("0");}
+		if(result.affectedRows>0){res.send("1");}
 		else{res,send("0");}
 	});
 });
@@ -48,7 +59,7 @@ router.delete('/v1/delfy/:fid',function(req,res){
 	});
 })
 //商品的添加路由： post  /v1/add
-router.post('/v1/add',function(req,res){
+router.post('/v1/proadd',function(req,res){
 	var obj=req.body;
 	var sql="insert into xz_laptop set ?";
 	pool.query(sql,[obj],function(err,result){
@@ -57,10 +68,16 @@ router.post('/v1/add',function(req,res){
 		else{res.send("0");}
 	});
 });
-//商品列表路由： post   /v1/list 
-router.post('/v1/list',function(req,res){
+//商品列表路由： post   /v1/prolist 
+router.post('/v1/prolist',function(req,res){
+	var $fid=req.body.fid;
+	if($fid==="not"){
 	var sql="select *from xz_laptop";
-	pool.query(sql,function(err,result){
+	}
+	else{
+	var sql="select *from xz_laptop where family_id=?";
+	}
+	pool.query(sql,[$fid],function(err,result){
 		if(err)throw err;
 		res.send(result);
 	});
@@ -75,20 +92,32 @@ router.post('/v1/plist',function(req,res){
 		res.send(result);
 	});
 });
-//商品删除：delete  /v1/delproduct/:lid
-router.delete('/v1/delproduct/:lid',function(){
+//商品删除：delete  /v1/delpro/:lid
+router.delete('/v1/delpro/:lid',function(req,res){
 	var $lid=req.params.lid;
-	var sql="delete xz_laptop where lid=?";
+	var sql="delete from xz_laptop where lid=?";
 	pool.query(sql,[$lid],function(err,result){
 		if(err)throw err;
 		if(result.affectedRows>0){res.send("1");}
 		else{res.send("0");}
 	});
 });
+//商品详情： get  /v1/details/:lid
+router.get('/v1/detail/:lid',function(req,res){
+	var $lid=req.params.lid;
+	console.log($lid);
+	var sql="select *from xz_laptop where lid=?";
+	pool.query(sql,[$lid],function(err,result){
+		if(err)throw err;
+		if(result.length>0){res.send(result);}
+		else{res.send("0");}
+	});
+});
 //商品修改： put  /v1/put
-router.put('/v1/put',function(req,res){
+router.put('/v1/uppro',function(req,res){
 	var obj=req.body;
 	var $lid=obj.lid;
+	console.log(obj,$lid);
 	    delete obj.lid;
 	var sql="update xz_laptop set ? where lid=?";
 	pool.query(sql,[obj,$lid],function(err,result){
